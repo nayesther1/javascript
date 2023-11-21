096a0189ab181956b0f9d27f19d048d38ed74101
nayiturikiesther <nayesther1@gmail.com>
2023-11-21 16:49:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//Finding a Remainder in JavaScript
+const remainder = 11 % 3;
+
+//Compound Assignment With Augmented Addition
+
+let a = 3;
+let b = 17;
+let c = 12;
+a += 12;
+b += 9 ;
+c += 7;
+
+//Compound Assignment With Augmented Subtraction
+let a = 11;
+let b = 9;
+let c = 3;
+
+a -= 6;
+b -=15;
+c -= 1;
+//Compound Assignment With Augmented Multiplication
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+//Compound Assignment With Augmented Division
+let a = 48;
+let b = 108;
+let c = 33;
+
+a /= 12;
+b /=  4;
+c /=  11;
